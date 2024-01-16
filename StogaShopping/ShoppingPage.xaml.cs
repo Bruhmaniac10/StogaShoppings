0c83ddb76756182e91c7f3f4d788fa31cb26b79e
Bruhmaniac10 <133171591+Bruhmaniac10@users.noreply.github.com>
2024-01-16 17:24:01 -0500

    shoppingpage updates

--- StogaShopping/ShoppingPage.xaml.cs
+++ StogaShopping/ShoppingPage.xaml.cs
@@ -1,9 +1,25 @@
-﻿namespace StogaShopping;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 
-public partial class ShoppingPage : ContentPage
+namespace StogaShopping
 {
-	public ShoppingPage()
-	{
-		InitializeComponent();
-	}
+    public class ProductsViewModel
+    {
+        public ObservableCollection<Product> Products { get; set; }
+
+        public ProductsViewModel()
+        {
+            // Initialize your Products collection with data from your database
+            Products = new ObservableCollection<Product>
+            {
+                new Product { ProductId = 1, Name = "Product 1", Seller = "Seller A", Price = 50, Description = "Description 1", Availability = true },
+                new Product { ProductId = 2, Name = "Product 2", Seller = "Seller B", Price = 30, Description = "Description 2", Availability = false },
+            };
+
+            // Shuffle the products randomly
+            Random rand = new Random();
+            Products = new ObservableCollection<Product>(Products.OrderBy(p => rand.Next()));
+        }
+    }
 }
