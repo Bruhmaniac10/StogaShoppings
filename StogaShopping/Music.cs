181bd8210b9d103bffaeebd9b0e10721ec4619b5
HawkSK2497 <102840154+HawkSK2497@users.noreply.github.com>
2024-01-18 21:57:05 -0500

    Add files via upload

---
+++ StogaShopping/Music.cs
@@ -0,0 +1,33 @@
+﻿using Plugin.Maui.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StogaShopping
+{
+    public class Music
+    {
+        public string musicval {  get; set; }
+
+        public int musicID { get; set; }
+
+        private readonly IAudioManager audioManager;
+
+        public Music(IAudioManager audioManager)
+        {
+            this.audioManager = audioManager;
+        }
+
+        public async void play()
+        {
+
+            var player = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync(musicval));
+
+            player.Play();
+
+        }
+
+    }
+}
---
+++ StogaShopping/MusicDatabase.cs
@@ -0,0 +1,60 @@
+﻿using Plugin.Maui.Audio;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StogaShopping
+{
+    public class MusicDatabase
+    {
+
+        SQLiteAsyncConnection Database;
+
+
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLite.SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+
+        }
+
+        public async Task<List<Music>> GetMusicAsync()
+        {
+            await Init();
+            return await Database.Table<Music>().ToListAsync();
+        }
+
+        /*	public async Task<List<User>> GetUsersNotDoneAsync()
+            {
+                await Init();
+                return await Database.Table<User>().Where(t => t.Done).ToListAsync();
+            }*/
+
+        public async Task<Music> GetMusicAsync(string val)
+        {
+            await Init();
+            return await Database.Table<Music>().Where(i => i.musicval == val).FirstOrDefaultAsync();
+        }
+
+        public async Task<int> SaveMusicAsync(Music music)
+        {
+            await Init();
+            if (music.musicID != 0)
+                return await Database.UpdateAsync(music);
+            else
+                return await Database.InsertAsync(music);
+        }
+
+        public async Task<int> DeleteMusicAsync(Music music)
+        {
+            await Init();
+            return await Database.DeleteAsync(music);
+        }
+
+    }
+}
